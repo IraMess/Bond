2e805636311b65fe95c017e6b15d31d5f33af6de
AlexPutimtsev <36736970+AlexPutimtsev@users.noreply.github.com>
2018-05-16 22:55:19 +0300

    BondModel package was deleted

--- src/main/java/com/bond/model/BondModel.java
+++
@@ -1,5 +0,0 @@
-package com.bond.model;
-
-public class BondModel {
-
-}
---
+++ src/main/java/com/bond/сrud/entities/RecordsClientCrud.java
@@ -0,0 +1,10 @@
+package com.bond.сrud.entities;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="records")
+public class RecordsClientCrud {
+
+	
+	
+}
